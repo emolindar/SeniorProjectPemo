bd1e0d7ba8726f4b4b9a761227d64d011f704a07
emolindar <emolindar@gmail.com>
2026-09-22 20:59:33 +0300

    more from 22.09

--- Project Folder/SeniorProjectPemo/src/Image.h
+++ Project Folder/SeniorProjectPemo/src/Image.h
@@ -1,10 +1,25 @@
 #include <vector>
-
+using namespace std;
 class Image{
 public:
-	Image(width, height) : width_(width), height_(height) {}
+	Image(int width,int height) : width_(width), height_(height) {}
 	int width(){return width_;}
 	int height(){return height_;}
-	vector<vector<float>> image_data(){return image_data_;}	
+	vector<Pixel> image_data(){return image_data_;}
 
-}
+private: 
+	int width_ = 0;
+	int height_ =0;
+	vector<Pixel> image_data_;
+};
+
+
+
+class Pixel{
+public:
+	Pixel(float red, float green, float blue, float opacity){}
+
+private: 
+	vector<float> rgb_; 
+	float opacity_; 
+};
